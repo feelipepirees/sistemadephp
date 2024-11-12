71ba8dde185ba1755bb8d998867796b29cc040e6
FELIPE PIRES DOS SANTOS <felipe.psantos48@senacsp.edu.br>
2024-11-11 22:09:48 -0300

    Quarto Commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+    namespace PHP\Modelo;
+    require_once('DAO\inserir.php');
+    require_once('DAO\Conexao.php');
+    use PHP\Modelo\DAO\Inserir;
+    use PHP\Modelo\DAO\Conexao;
+?>
+
+<!Doctype Html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Página  Principal</title>
+
+</head>
+<body>
+    <form method="POST">
+        <label>Codigo: </label><br>
+        <input type="number" name="codigo" id="codigo"><br><br>
+
+        <label>Nome: </label><br>
+        <input type="text" name="nome" id="nome"><br><br>
+
+        <label>Endereço: </label><br>
+        <input type="text" name="endereco" id="endereco"><br><br>
+
+        <label>telefone: </label><br>
+        <input type="text" name="telefone" id="telefone"><br><br>
+
+        <button type=submit>Cadastrar
+        <?php
+            
+            $conexao = new Conexao();//Conectando o banco
+
+            //pegar os dados do formulário
+            $codigo   = $_POST['codigo'];
+            $nome     = $_POST['nome'];
+            $endereco = $_POST['endereco'];
+            $telefone = $_POST['telefone'];
+            //chamar a classe inserir
+            $inserir = new Inserir();
+            echo $inserir->cadastrarPessoa($conexao,$codigo,$nome,$endereco,$telefone);
+        ?>
+        </button>
+
+ </form><!--Fim do Formulario--> 
+
+
+</body>
+
+</html>
--- main.php
+++ main.php
@@ -1,23 +1,19 @@
 <?php
     namespace PHP\Modelo; //Definir o Local do Projeto
 
-    require_once('Paciente.php'); //Requisitando uma Classe
     require_once('Pessoa.php');
-    require_once('Recepcionista.php');
-    require_once('Medico.php');
-    require_once('Endereco.php');
     require_once('DAO/Conexao.php');
     require_once('DAO/Inserir.php');
     require_once('DAO/Consultar.php');
-    use PHP\Modelo\Cliente;//Definir a classe
-    use PHP\Modelo\Paciente; 
+    require_once('DAO/Atualizar.php');
+    require_once('DAO/Excluir.php');
     use PHP\Modelo\Pessoa;
-    use PHP\Modelo\Recepcionista;
-    use PHP\Modelo\Medico;
-    use PHP\Modelo\Endereco;
     use PHP\Modelo\DAO\Conexao;
     use PHP\Modelo\DAO\Inserir;
     use PHP\Modelo\DAO\Consultar;
+    use PHP\Modelo\DAO\Atualizar;
+    use PHP\Modelo\DAO\Excluir;
+
     /*
 
     $endCliente = new Endereco("Avenida","400","Centro","São Bernardo do Campo","São Paulo","Brasil","09950-000");
@@ -43,4 +39,11 @@
 
     $consultar = new Consultar();
     echo $consultar->consultarIndividual($conn,'1');
+    
+    $atualizar = new Atualizar();
+    echo $atualizar->atualizarPessoa($conn, "nome", "allan",1);
+
+    $excluir = new Excluir();
+    $excluir->excluirPessoa($conn,1);
+
 ?>
